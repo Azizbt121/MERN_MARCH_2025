3c07f1057c88cd90c9add624a9d813a3aede858d
Azizbt121 <bentalebaziz577@gmail.com>
2025-06-07 15:24:46 +0100

    routing

---
+++ project10/src/components/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => (
+  <>
+    <h1 style={{ fontSize: '3rem' }}>Welcome to Home Page</h1>
+  </>
+);
+
+export default Home;
---
+++ project10/src/components/Number.jsx
@@ -0,0 +1,5 @@
+const Number = () => (
+  <h1 style={{ fontSize: '3rem' }}> The number is : 4</h1>
+);
+
+export default Number;
